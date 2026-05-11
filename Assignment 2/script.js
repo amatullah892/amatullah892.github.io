98e1753475dd0c1121986e35f05c5395b0ccdaec
amatullah892 <s4144544@student.rmit.edu.au>
2026-05-11 10:17:20 +1000

    assignment2

--- Assignment 2/script.js
+++ Assignment 2/script.js
@@ -1,1 +1,21 @@
+const track = [
+    {
+    title:"Chill FM"
+    artist:"Dope Cat"
+    src:"https://uppbeat.io/t/dope-cat/chill-fm"
+},
+{
+    title:"All Of My"
+    artist:"Pryces"
+    src:"https://uppbeat.io/t/pryces/all-of-my"
+},
+{
+    title:"Golden Age"
+    artist:"Walz"
+    src:"https://uppbeat.io/t/walz/golden-age"
+}
+]
 
+const icons = {
+    play:
+}
